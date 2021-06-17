917ffdddfca4b07d24a8a4d8f02ef1ee013d5deb
jameskokoska <jameskokoska@users.noreply.github.com>
2021-06-17 17:08:51 -0400

    added all source, updated readme

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # DesignWebsite
-👋 A resume like website following different design rules. Designed around animations and a modern clean style.
+A resume like website following different design rules. Designed around animations and a modern clean style.
+
+## Features
+* Read data from an a database array (located in ```data.js```)
+* Utilize CSS variables for easy change of colour palette
+* Universal design for all components
+* Modern, clean, and fluid UI
+* Fluid entrance and hover animations
+* Mobile and all screen size support using responsive containers
+## Images
+![Main menu](/preview/main.png)
+![Content](/preview/content1.png)
+![Content](/preview/content2.png)
+![Mobile Preview](/preview/mobile1.png)
+![Mobile Preview](/preview/mobile2.png)
+![Mobile Preview](/preview/mobile3.png)
---
+++ data.js
@@ -0,0 +1,68 @@
+var content = {
+  "websiteTitle":"Welcome!",
+  "firstName":"John",
+  "lastName":"Smith",
+  "aboutMe":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quam massa, dapibus a cursus et, scelerisque sit amet lacus. Praesent id sapien neque. Phasellus imperdiet felis ac quam hendrerit, sed finibus massa fringilla. s dui quis, lacinion vulputate dolor.",
+  "email":"test@test.ca",
+  "linkedIn":"https://www.google.com",
+  "resumeLink":"https://www.google.com",
+  "license":"Icons from FontAwesome, Header image from Unsplash",
+  "experience":[
+    {
+      "picture":"./assets/university-solid.svg",
+      "title":"University",
+      "text":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quam massa, dapibus a cursus et, scelerisque sit amet lacus.  Phasellus id tempus ex, sit amet tristique felis. Morbi placerat luctus vestibulum. Vivamus blandit arcu sit amet libero lobortis dictum. Vivamus vitae leo nibh. Sed ac tortor sagittis, egestas dui quis, lacinia justo. Ut sed arcu in orci maximus bibendum vel sed nunc. Sed non vulputate dolor.",
+    },
+    {
+      "picture":"./assets/briefcase-solid.svg",
+      "title":"Work",
+      "text":"Lorem ipsum dolor sit amet, Phasellus nec nunc a turpis tempor interdum. Nullam condimentum consequat pretium. Nullam pretium rutrum urna, sit amet pretium augue aliquet at. Phasellus id tempus ex, sit amet tristique felis. Morbi placerat luctus vestibulum. Vivamus blandit arcu sit amet libero lobortis dictum. Vivamus vitae leo nibh. Sed ac tortor sagittis, egestas dui quis, lacinia justo. Ut sed arcu in orci maximus bibendum vel sed nunc. Sed non vulputate dolor.",
+    },
+    {
+      "title":"Test",
+      "text":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quam massa, dapibus a cursus et, scelerisque sit amet lacus. Praesent id sapien neque. Phasellus imperdiet felis ac quam hendrerit, sed finibus massa fringilla. Aenean blandit sapien id quam cursus, et luctus magna ultrices. Etiam id erat quis diam vestibulum mollis. Cras tempor elit non risus feugiat commodo. Phasellus nec nunc a turpis tempor interdum. Nullam condimentum consequat pretium. Nullam pretium rutrum urna, sit amet pretium augue aliquet at. Phasellus id tempus ex, sit amet tristique felis. Morbi placerat luctus vestibulum. Vivamus blandit arcu sit amet libero lobortis dictum. Vivamus vitae leo nibh. Sed ac tortor sagittis, egestas dui quis, lacinia justo. Ut sed arcu in orci maximus bibendum vel sed nunc. Sed non vulputate dolor.",
+    },
+    {
+      "picture":"./assets/map-marker-alt-solid.svg",
+      "title":"Test 2",
+      "text":"Lorem ipsum dolor sit amet,  Vivamus vitae leo nibh. Sed ac tortor sagittis, egestas dui quis, lacinia justo. Ut sed arcu in orci maximus bibendum vel sed nunc. Sed non vulputate dolor.",
+    },
+    {
+      "title":"Test 3",
+      "text":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quam massa, dapibus a cursus et, scelerisque sit amet lacus. Praesent id sapien neque. Phasellus imperdiet felis ac quam hendrerit, sed finibus massa fringilla. Aenean blandit sapien id quam cursus, et luctus magna ultrices. Etiam id erat quis diam vestibulum mollis. Cras tempor elit non risus feugiat commodo.  Sed non vulputate dolor.",
+    },
+    {
+      "title":"Test 4",
+      "text":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quam massa, dapibus a cursus et, scelerisque sit amet lacus. Praesent id sapien neque. Phasellus imperdiet felis ac quam hendrerit, sed finibus massa fringilla. Aenean blandit sapien id quam cursus, et luctus magna ultrices. Etiam id erat quis diam vestibulum mollis. Cras tempor elit non risus feugiat commodo.  Sed non vulputate dolor.",
+    },
+    {
+      "title":"Test 5",
+      "text":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quam massa, dapibus a cursus et, scelerisque sit amet lacus. Praesent id sapien neque. Phasellus imperdiet felis ac quam hendrerit, sed finibus massa fringilla. Aenean blandit sapien id quam cursus, et luctus magna ultrices. Etiam id erat quis diam vestibulum mollis. Cras tempor elit non risus feugiat commodo.  Sed non vulputate dolor.",
+    },
+  ],
+  "skills":[
+    {
+      "picture":"./assets/university-solid.svg",
+      "title":"University",
+      "text":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quam massa, dapibus a cursus et, scelerisque sit amet lacus.  ",
+    },
+    {
+      "picture":"./assets/briefcase-solid.svg",
+      "title":"Work",
+      "text":"Lorem ipsum dolor sit amet, Phasellus nec nunc a turpis tempor interdum. ",
+    },
+    {
+      "title":"Test",
+      "text":"Maecenas quam massa, dapibus a cursus et, scelerisque sit amet lacus. ",
+    },
+    {
+      "picture":"./assets/map-marker-alt-solid.svg",
+      "title":"Test 2",
+      "text":"Lorem ipsum dolor sit amet,  Vivamus vitae leo nibh. Sed ac tortor sagittis, egestas dui quis, lacinia justo. Ut sed arcu in orci maximus bibendum vel sed nunc. Sed non vulputate dolor.",
+    },
+    {
+      "title":"Test 3",
+      "text":"Lorem ipsum dolor sit amet,  Vivamus vitae leo nibh. Sed ac tortor sagittis, egestas dui quis, lacinia justo. Ut sed arcu in orci maximus bibendum vel sed nunc. Sed non vulputate dolor.",
+    },
+  ],
+}
